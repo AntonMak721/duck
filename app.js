f82e499f5a6684faa8379da49c258a6d3af20ebd
Anton Mak <Anton3841@yandex.ru>
2025-03-22 00:32:04 +0300

    add adaptive

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log('я родился!')
+
+const popup = document.getElementById('popup')
+const buttonPopup= document.getElementById('buttonPopup')
+const popupClose = document.getElementById('popupClose')
+const overlay = document.getElementById('overlay');
+console.log(popup)
+const bodyEl = document.body;
+
+
+buttonPopup.addEventListener('click', function(){
+    popup.classList.remove('none');
+    console.log('click')
+    overlay.style.display = 'flex';
+    // bodyEl.classList.toggle("noscroll")
+})
+
+popupClose.addEventListener('click', function(){
+    popup.classList.toggle('none');
+    overlay.style.display = 'none';
+})
+overlay.addEventListener('click', function(event) {
+    if (event.target === overlay) {
+        overlay.style.display = 'none';
+    }
+    popup.classList.toggle('none');
+});
